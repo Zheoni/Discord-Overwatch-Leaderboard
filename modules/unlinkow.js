5b84e2d1b1f03b954afc7dee0a4bf1c96270f534
Fran <zheoni@outlook.es>
2018-07-22 23:56:54 +0200

    Added the unlink command

---
+++ modules/unlinkow.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+const functions = require('./functions.js');
+
+module.exports.run = async (bot, message, args) => {
+    let owdata = functions.loadData('owdata.json');
+    if(owdata[message.author.id]){
+        await delete owdata[message.author.id];
+        await functions.saveData(owdata, 'owdata.json');
+        return message.reply('You unlinked your btaf succesfully');
+    }else return message.reply('You have not linked your btag');
+}
+
+module.exports.help = {
+    name: 'unlinkow',
+    command: true
+}
