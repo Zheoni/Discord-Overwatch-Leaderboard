bfb4f6b4f4f26b7649c9d8363a30915744a8df51
Fran <zheoni@outlook.es>
2018-07-28 03:30:25 +0200

    Reduced time between read and save data, risk minimized but not solved

--- modules/linkow.js
+++ modules/linkow.js
@@ -7,15 +7,10 @@ module.exports.run = async (bot, message, args) => {
   const region = args[1].toLowerCase();
   const btag = args[2].replace("#", "-");
 
-  let owdata = functions.loadData('owdata.json');
-
-  let guildplayers = {};   //auxiliar object of all the players in the server
-  if (owdata[message.guild.id]) guildplayers = owdata[message.guild.id];
-
   //verify the args
   if (platform != 'pc') return await message.reply("Sorry, the bot only support 'pc' player at this moment."); //because
   if (region != "eu" && region != "us" && region != "asia") return await message.reply("The region must be: 'eu', 'us' or 'asia'.");
-
+  
   //aks the api for the data
   try {
     await overwatch.getOverall(platform, region, btag).then((data) => {
@@ -30,18 +25,28 @@ module.exports.run = async (bot, message, args) => {
         }
       }
 
-      guildplayers[message.author.id] = player; //add it to the rest
-
-      owdata[message.guild.id] = guildplayers;  //add the server to the rest
-
+      addPlayer(player);
+      
       message.reply(`You linked "${btag.replace("-", "#")}" in "${platform}" in "${region}" to your discord accounthis bot. If it  is not correct, try the command again.`);
-
-      functions.saveData(owdata, 'owdata.json');
+      
       console.log(`linkow success: ${btag} ${message.author.username}  ${data.profile.rank}sr`);
     });
   } catch (err) {
     console.log(err);
     return message.reply('The profile was not found... or some other weird error. Try again later.');
+  }
+  
+  function addPlayer(player) {
+    let owdata = functions.loadData('owdata.json');
+    
+    let guildplayers = {};   //auxiliar object of all the players in the server
+    if (owdata[message.guild.id]) guildplayers = owdata[message.guild.id];
+    
+    guildplayers[message.author.id] = player; //add it to the rest
+    
+    owdata[message.guild.id] = guildplayers;  //add the server to the rest
+    
+    functions.saveData(owdata, 'owdata.json');
   }
 
 }
