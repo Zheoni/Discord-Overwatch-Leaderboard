a5c2d55afadb71294a673106a5a7039cb200bac6
Francisco José Sánchez Moreno <zheoni@outlook.es>
2019-03-04 18:13:30 +0100

    Unlinkow module done

--- modules/unlinkow.js
+++ modules/unlinkow.js
@@ -1,21 +1,29 @@
 const Discord = require('discord.js');
-
-const functions = require('./functions.js');
+const { Accounts, Leaderboards } = require("../dbObjects");
 
 module.exports.run = async (bot, message, args) => {
-    let owdata = functions.loadData('owdata.json');
-    if (owdata[message.guild.id]) {
-        if (owdata[message.guild.id][message.author.id]) {
-            await delete owdata[message.guild.id][message.author.id];
-            await functions.saveData(owdata, 'owdata.json');
-            return message.reply('You unlinked your btag succesfully');
-        } else return message.reply('You have not linked your btag');
-    } else return message.reply('This server has no one with its profile linked, not even you!');
+    if (args[0]) {
+        Leaderboards.destroy({where: {
+            guild_id: message.guild.id,
+            user_id: message.author.id,
+            btag: args[0].replace('#', '-')
+        }});
+        Leaderboards.findAndCount({where: {
+            btag: args[0].replace('#', '-')
+        }}).then((result) => {
+            if (result.count == 0) {
+                Accounts.destroy({where: {
+                        battleTag: args[0].replace('#', '-')
+                    }
+                })
+            }
+        });
+    } else return message.reply(usage);
 }
 
 module.exports.help = {
     name: 'unlinkow',
     command: true,
-    usage: "unlinkow",
+    usage: "unlinkow [battletag]; or: unlinkow all",
     description: "Delete your data and you will no longer appear in the leaderboard."
 }
