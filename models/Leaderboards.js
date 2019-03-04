f81872db9c35dd16ad16566560058eed381b7e68
Francisco José Sánchez Moreno <zheoni@outlook.es>
2019-03-04 17:47:27 +0100

    Finished linkow module.

--- models/Leaderboards.js
+++ models/Leaderboards.js
@@ -2,11 +2,18 @@ module.exports = (sequelize, DataTypes) => {
     return sequelize.define('leaderboards', {
         guild_id: {
             type: DataTypes.STRING,
-            primaryKey: true
+            primaryKey: true,
+            allowNull: false
         },
         btag: {
             type: DataTypes.STRING,
-            primaryKey: true
+            primaryKey: true,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false
         },
         username: DataTypes.STRING
     }, {
--- modules/linkow.js
+++ modules/linkow.js
@@ -1,58 +1,66 @@
 const Discord = require('discord.js');
-const fetch = require('node-fetch');
-const functions = require("./functions.js");
+const api = require("../overwatchData");
+const { Accounts, Servers, Leaderboards } = require("../dbObjects");
 
 module.exports.run = async (bot, message, args) => {
 	let platform, region, btag;
-	if(args[0] && args[1] && args[2]){
+	if (args[0] && args[1] && args[2]) {
 		platform = args[0].toLowerCase();
 		region = args[1].toLowerCase();
 		btag = args[2].replace("#", "-");
-	}else return await message.reply("Make sure you follow thr scheme !linkow <platflom> <region> <btag>");
+	} else return await message.reply("Make sure you follow thr scheme !linkow <platflom> <region> <btag>");
 
 	//verify the args
-	if (platform != 'pc') return await message.reply("Sorry, the bot only support 'pc' player at this moment."); //because
-	if (region != "eu" && region != "us" && region != "asia") return await message.reply("The region must be: 'eu', 'us' or 'asia'.");
+	if (platform != 'pc')
+		return await message.reply("Sorry, the bot only support 'pc' player at this moment."); //because
+	if (region != "eu" && region != "us" && region != "asia")
+		return await message.reply("The region must be: 'eu', 'us' or 'asia'.");
 
-	const link = `http://ovrstat.com/stats/${platform}/${region}/${btag}`;
-	
 	//aks the api for the data
-
-	try {
-		let data = await fetch(link).then((response) => response.json());
-		//create the player
-		let player = {
-			username: message.author.tag,
-			platform: platform,
-			region: region,
-			battleTag: btag,
-			overwatch: {
-				rank: data.rating
-			}
-		}
-		//console.log(data);
-		addPlayer(player);
-		let embed = createEmbed(data);
-		
-		message.reply({embed:embed});
-		
-		console.log(`linkow success: ${btag} ${message.author.username}  ${data.rating}sr`);
-	} catch (err) {
-		console.log(err);
+	const data = await api.fetchAPI(btag, platform, region).then((data) => {
+		return data;
+	}).catch((error) => {
+		console.error(error);
+		console.log("Cannot fetch " + players[i].battleTag);
 		return message.reply('The profile was not found... or some other weird error. Try again later.');
+	});
+	
+	const added = await addPlayer(btag, platform, region, data.rating);
+	if (added) {
+		let embed = createEmbed(data);
+		console.log(`linkow success: ${btag} ${message.author.username}  ${data.rating}sr`);
+		await message.reply({ embed: embed });
+	} else {
+		await message.reply("The leaderboard does not allow multiple accounts, ask your server admind to change that.");
 	}
 	
-	function addPlayer(player) {
-		let owdata = functions.loadData('owdata.json');
+	async function addPlayer(btag, platform, region, rank) {
+		const canMultiple = await Servers.findByPk(message.guild.id).then((guild) => {
+			return guild.lbAllowMultiple;
+		});
 		
-		let guildplayers = {};   //auxiliar object of all the players in the server
-		if (owdata[message.guild.id]) guildplayers = owdata[message.guild.id];
-		
-		guildplayers[message.author.id] = player; //add it to the rest
-		
-		owdata[message.guild.id] = guildplayers;  //add the server to the rest
-		
-		functions.saveData(owdata, 'owdata.json');
+		const count = await Leaderboards.findAndCount({where: {
+			guild_id: message.guild.id,
+			user_id: message.author.id,
+		}}).then((result) => {
+			return result.count;
+		});
+
+		if(count == 0 || canMultiple) {
+			Accounts.upsert({
+				battleTag: btag,
+				platform: platform,
+				region: region,
+				rank: rank
+			});
+			Leaderboards.upsert({
+				guild_id: message.guild.id,
+				btag: btag,
+				user_id: message.author.id,
+				username: message.author.username
+			});
+			return true;
+		} else return false;
 	}
 
 }
@@ -68,8 +76,8 @@ function createEmbed(data) {
 	let embed = new Discord.RichEmbed()
 		.setAuthor(data.name, data.ratingIcon)
 		.setThumbnail(data.icon)
-		.setDescription('You linked your ow profile with the bot, in the next leaderboard update you will appear with' + 
+		.setDescription('You linked your ow profile with the bot, in the next leaderboard update you will appear with' +
 			' a rank of **' + data.rating + '**')
-		.addField('Note:','If you want to change the linked account, run the command again')
+		.addField('Note:', 'If you want to change the linked account, run the command again')
 	return embed;
 }
